3c564c7d5c8d7bfeacb5bd2770d069ac4ae272da
dania istanboli <dania.ist.mi96@gmail.com>
2022-09-15 02:57:08 +0300

    setup server

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+// express app
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+mongoose.connect(process.env.MONGO_DB_URL).then(() => {
+  app.listen(PORT, () => {
+    console.log("server is working");
+  });
+});
